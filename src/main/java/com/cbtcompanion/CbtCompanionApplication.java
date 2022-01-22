283292c1b5cf50a1269627f03a72472ba943c07b
Divya <pai.divya03@gmail.com>
2022-01-22 14:11:33 +0530

    Initial commit

---
+++ src/main/java/com/cbtcompanion/CbtCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.cbtcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbtCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbtCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbtcompanion/CbtCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbtcompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbtCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
